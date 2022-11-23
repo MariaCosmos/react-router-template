4481c53822506ada28ee93aa28922d10fdf8ee1d
MariaCosmos <cosmos.maria2003@gmail.com>
2022-11-23 11:03:32 -0300

    exercicios extra - React Router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,21 @@
+import {useNavigate} from "react-router-dom"
+import {goToHomePage, goToProfilePage, goToLoginPage} from "../routers/coordinator"
+
 function Header() {
+  const navigate = useNavigate()
+
 
   return (
     <header>
-      <button >
+      <button onClick={() => goToHomePage(navigate)}>
         Ir para página inicial
       </button>
-      <button >
+      <button onClick={() => goToProfilePage(navigate, "Caos")}>
         Ir para página de perfil
       </button>
+      <button onClick={() => goToLoginPage(navigate)}>
+        Ir para página de login
+      </button>      
     </header>
   );
 }
